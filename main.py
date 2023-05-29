2711bd80b014dac40a60b8a9ee2b870002488517
Elienay Alves <elienay8486@gmail.com>
2023-05-29 12:52:46 -0300

    feat(main.py): implement band generator

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Greetings to the Band Name Generator")
+city = input("What is the city you grew up?\n")
+pet = input("What is the name of your pet?\n").lower()
+
+band_name = city + pet
+print(f"A great name for your band could be: {band_name}!")
